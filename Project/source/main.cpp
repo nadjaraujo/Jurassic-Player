91f148ba1d49feaef56f770fc19d425f36801d55
claudiosouzabrito <36930457+claudiosouzabrito@users.noreply.github.com>
2020-03-20 20:52:58 -0300

    Update main.cpp

--- Project/source/main.cpp
+++ Project/source/main.cpp
@@ -64,7 +64,7 @@ int main()
     std::vector<float> positions = {0.5f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f, -0.5f, 0.5f, 0.0f};
     std::vector<unsigned int> indices = {0, 1, 3, 1, 2, 3};
     std::vector<float> textureCoords = {1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
-    Model modelo = Model(loader.loadVAO(vertices, indices, textureCoords));
+    Model modelo = Model(loader.loadVAO(vertices, indices));
     Renderer renderer = Renderer();
     unsigned int texture = loader.loadTexture("Textura/container.jpg");
     std::vector<Model> modelos;
