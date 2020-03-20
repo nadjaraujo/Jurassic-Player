e2d051e17d4fd03bb158009a1a128a6330ed9f18
claudiosouzabrito <36930457+claudiosouzabrito@users.noreply.github.com>
2020-03-20 20:36:29 -0300

    Update main.cpp

--- Project/source/main.cpp
+++ Project/source/main.cpp
@@ -18,7 +18,7 @@ std::vector<float> vertices= {
      0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
      0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
     -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 1.0f  // top left
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
 };
 
 int main()
