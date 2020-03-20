24fbd99240a6e85615ed8e1116e989d4c9ad6f42
claudiosouzabrito <36930457+claudiosouzabrito@users.noreply.github.com>
2020-03-20 20:30:56 -0300

    Update functions.hpp

--- Project/source/functions.hpp
+++ Project/source/functions.hpp
@@ -29,23 +29,27 @@ void glwindowhint()
 //FUNCOES REFERENTES A TRIANGULOS
 
 //fonte dos shaders dos vertices
-const char *vertexShaderSource = "#version 400 core\n"
-                                 "layout (location = 0) in vec3 aPos;\n"
-                                 "layout (location = 1) in vec3 aColor;\n"
-                                 "out vec3 ourColor;\n"
-                                 "void main()\n"
-                                 "{\n"
-                                 "   gl_Position = vec4(aPos, 1.0);\n"
-                                 "ourColor = aColor;\n"
-                                 "}\0";
+const char *vertexShaderSource = "#version 330 core\n"
+                                "layout (location = 0) in vec3 aPos;\n"
+                                "layout (location = 1) in vec3 aColor;\n"
+                                "layout (location = 2) in vec2 aTexCoord;\n"
+                                "out vec3 ourColor;\n"
+                                "out vec2 TexCoord;\n"
+                                "void main()\n"
+                                "{\n"
+                                    "gl_Position = vec4(aPos, 1.0);\n"
+                                    "ourColor = aColor;\n"
+                                    "TexCoord = aTexCoord;\n"
+                                "}\0";
 
 const char *fragmentShaderSource = "#version 400 core\n"
                                    "out vec4 FragColor;\n"
                                    "in vec3 ourColor;\n"
-                                   "uniform vec4 OurColor;\n"
+                                   "in vec2 TexCoord;\n"
+                                   "uniform sampler2D ourTexture;\n"
                                    "void main()\n"
                                    "{\n"
-                                   "   FragColor = vec4 (ourColor, 1.0);\n"
+                                   "   FragColor = texture(ourTexture, TexCoord);\n"
                                    "}\n\0";
 
 void glVertexsourcecompile(int vertexShader)
